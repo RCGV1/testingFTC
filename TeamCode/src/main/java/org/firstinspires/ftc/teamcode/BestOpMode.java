ba9114207f695d616b56f0414fd4a46ea65434be
benjaminfaershtein <Benjaminfaer@gmail.com>
2023-08-16 21:02:15 -0700

    change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BestOpMode.java
@@ -1,19 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.arcrobotics.ftclib.drivebase.MecanumDrive;
-import com.arcrobotics.ftclib.gamepad.ButtonReader;
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.arcrobotics.ftclib.hardware.motors.MotorEx;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.commands.DriveCommand;
 import org.firstinspires.ftc.teamcode.commands.DriveSlowCommand;
 
 @TeleOp(name = "Best OpMode")
-public class bestOpMode extends BaseDriveChassis {
+public class BestOpMode extends BaseDriveChassis {
     private DriveCommand driveCommand;
     private DriveSlowCommand driveSlowCommand;
 
